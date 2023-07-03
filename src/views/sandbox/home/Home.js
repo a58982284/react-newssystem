d55518df35a708f777711eafa1375a31a651bece
yil8 <yil8@cisco.com>
2023-07-04 06:52:07 +0800

    piechart

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState,useRef } from 'react'
-import { Card, Col, Row, List, Avatar } from 'antd';
+import React, { useEffect, useState, useRef } from 'react'
+import { Card, Col, Row, List, Avatar, Drawer } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import axios from 'axios'
 import * as Echarts from 'echarts'
@@ -11,8 +11,11 @@ const { Meta } = Card;
 export default function Home() {
     const [viewList, setviewList] = useState([])
     const [starList, setstarList] = useState([])
-
+    const [allList, setallList] = useState([])
+    const [visible, setvisible] = useState(false)
+    const [pieChart, setpieChart] = useState(null)
     const barRef = useRef()
+    const pieRef = useRef()
     useEffect(() => {
         axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res => {
             // console.log(res.data)
@@ -29,17 +32,18 @@ export default function Home() {
 
     useEffect(() => {
 
-        axios.get("/news?publishState=2&_expand=category").then(res=>{
+        axios.get("/news?publishState=2&_expand=category").then(res => {
             // console.log(res.data)
             // console.log()
-            renderBarView(_.groupBy(res.data,item=>item.category.title))
+            renderBarView(_.groupBy(res.data, item => item.category.title))
+            setallList(res.data)
         })
-        return ()=>{
-            window.onresize=null
+        return () => {
+            window.onresize = null
         }
     }, [])
 
-    const renderBarView = (obj)=>{
+    const renderBarView = (obj) => {
         var myChart = Echarts.init(barRef.current);
 
         // 指定图表的配置项和数据
@@ -53,26 +57,78 @@ export default function Home() {
             },
             xAxis: {
                 data: Object.keys(obj),
-                axisLabel:{
-                    rotate:"60"
+                axisLabel: {
+                    rotate: "60"
                 }
             },
             yAxis: {
-                minInterval:1
+                minInterval: 1
             },
             series: [{
                 name: '数量',
                 type: 'bar',
-                data: Object.values(obj).map(item=>item.length)
+                data: Object.values(obj).map(item => item.length)
             }]
         };
 
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
-        window.onresize=()=>{
+        window.onresize = () => {
             console.log("resize");
             myChart.resize()
         }
+    }
+
+    const renderPieView = (obj) => {
+        var  currentList = allList.filter(item=>item.author===username);
+        var groupObj = _.groupBy(currentList,item=>item.category.title)
+        var list =[]
+        for (var i in groupObj) {
+            list.push({
+                name:i,
+                value:groupObj[i].length
+            }
+            )
+            
+        }
+        var myChart;
+        if (!pieChart) {
+            myChart = Echarts.init(pieRef.current);
+            setpieChart(myChart)
+        }else{
+            myChart = pieChart
+        }
+        var option;
+        option = {
+            title: {
+                text: '当前用户新闻分类图示',
+                subtext: '纯属虚构',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'item'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left'
+            },
+            series: [
+                {
+                    name: '发布数量',
+                    type: 'pie',
+                    radius: '50%',
+                    data: list,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        option && myChart.setOption(option);
     }
 
     const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem("token"))
@@ -112,7 +168,13 @@ export default function Home() {
                             />
                         }
                         actions={[
-                            <SettingOutlined key="setting" />,
+                            <SettingOutlined key="setting" onClick={() => {
+                                setTimeout(()=>{
+                                    setvisible(true)
+                                    renderPieView()
+                                },0)
+
+                            }} />,
                             <EditOutlined key="edit" />,
                             <EllipsisOutlined key="ellipsis" />,
                         ]}
@@ -133,9 +195,19 @@ export default function Home() {
                 </Col>
             </Row>
 
+            <Drawer width="500px" title="个人新闻分类" placement="right" closable={true} onClose={() => {
+                setvisible(false)
+            }} open={visible}>
+                <div ref={pieRef} style={{
+                    width: '100%',
+                    height: "400px",
+                    marginTop: "30px"
+                }}></div>
+            </Drawer>
+
 
             <div ref={barRef} style={{
-                width:'100%',
+                width: '100%',
                 height: "400px",
                 marginTop: "30px"
             }}></div>
